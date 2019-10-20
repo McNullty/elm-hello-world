1df0142b2d4988a67b88e4ccb6dade00285ff468
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-20 12:56:46 +0200

    Gradle init

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+tasks.register<Exec>("build") {
+    val jsPath = "$projectDir/build/elm.js"
+
+    inputs.dir("$projectDir/src/")
+    outputs.file(file(jsPath))
+
+    workingDir = File("$projectDir/src/")
+    commandLine = listOf("elm", "make", "$projectDir/src/main/elm/Main.elm", "--output", jsPath )
+}
+
+tasks.register<Delete>("clean") {
+    delete.add("elm/elm-stuff/0.19.0")
+    delete.add("elm/elm-stuff/generated-code")
+    delete.add("build")
+}
