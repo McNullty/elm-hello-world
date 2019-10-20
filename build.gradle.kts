b9226a4aa04ca4815388532df2fb54c1160acc8c
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-20 13:19:54 +0200

    Configuring gradle tasks for running elm-live

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,19 @@ tasks.register<Exec>("build") {
     commandLine = listOf("elm", "make", "$projectDir/src/main/elm/Main.elm", "--output", jsPath )
 }
 
+tasks.register<Exec>("elmLive") {
+    workingDir = File("$projectDir/build/")
+    commandLine = listOf("elm-live", "src/main/elm/Main.elm", "--pushstate", "--open", "--", "--output=build/elm.js")
+
+    dependsOn("copyResources")
+}
+
+tasks.register<Copy>("copyResources") {
+
+    from(File("$projectDir/src/main/resources/"))
+    into(File("$projectDir/build/"))
+}
+
 tasks.register<Delete>("clean") {
     delete.add("elm/elm-stuff/0.19.0")
     delete.add("elm/elm-stuff/generated-code")
